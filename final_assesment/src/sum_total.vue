b5d24fb99426feba729dd6adfa438852cf74390e
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-12 13:44:24 +0530

    this vue file will give the total amount of cart

---
+++ final_assesment/src/sum_total.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="CartItem">
+       <h3>Cart_total: ${{cart_TA.toFixed(4)}}</h3>
+
+       <button class="view-pb"> Pay by card</button>
+
+     
+
+    </div>
+</template>
+<script>
+export default {
+    
+    computed:{
+        cart_TA(){
+            return this.$state.getters.cartTTA()
+        }
+    }
+}
+</script>>
