947b6d17e243d3c050bf332fafbd4ab5c9915207
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-12 13:47:40 +0530

    this one is the list of items written in js

---
+++ final_assesment/src/item.js
@@ -0,0 +1,41 @@
+const items = [
+    {
+        
+        id: 1,
+        name: 'Real Madrid ',
+        price: 4999,
+        category : 'Footbal Jersyes',
+        description: "real madrid"
+
+    },
+    {
+        
+        id: 2,
+        name: 'Manchester United',
+        price: 4999,
+        category : 'Footbal Jersyes',
+        description: "Manchester United"
+
+    },
+    {
+        
+        id: 3,
+        name: 'Juventus',
+        price: 4999,
+        category : 'Footbal Jersyes',
+        description: "real madrid"
+
+    },
+    {
+        
+        id: 4,
+        name: 'Bayern Munich',
+        price: 4999,
+        category : 'Footbal Jersyes',
+        description: "real madrid"
+
+    },
+ 
+
+]
+export default items
