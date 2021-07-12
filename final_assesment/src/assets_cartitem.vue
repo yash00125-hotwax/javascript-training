eb74fd0697df2dd034e2cba8de7248c79ca36642
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-12 13:43:18 +0530

    this vue cotains all items in the website

---
+++ final_assesment/src/assets_cartitem.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class = "cart-item">
+      <h3>{{product.name}}</h3>>
+      <h4>Jerseys in Cart: {{product.quantity}}</h4>
+      <h4>Total Amount: {{item_amount.toFixed(4)}}
+    
+    
+    </div>
+
+</template>>
+
+<script>
+export default {
+    props: ['product'],
+
+    computed:{
+        description(){
+            return this.product.desccription.substring
+
+        },
+        item_amount(){
+            return this.product.price * this.product.quantity
+        }
+    } 
+}
+</script>
+<style>
+.cart-item{
+    width:90%;
+    margin:10%;
+    background-color: khaki;
+    box-shadow: 0 0 5px grey;
+    border-radius: 5px;
+    padding: 10px;
+    text-align:left;
+}
+    .header{
+        display: flex;
+        justify-content :space-around;
+    }
+
+</style>
