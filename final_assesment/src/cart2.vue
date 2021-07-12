7f1f399de2085b0a8e1fd0e1b3a1e1ead0ba0678
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-12 13:51:08 +0530

    this page weill be  used to display puropse

---
+++ final_assesment/src/cart2.vue
@@ -0,0 +1,35 @@
+<template>
+   <div>
+     <h1>Cart</h1>
+
+
+    
+     <CartItem
+      v-for="product in items"
+      :key="product.id"
+      :product="product"
+      /> 
+
+      <Summary
+      
+      />
+
+
+   </div>
+
+</template>
+
+<script>
+import CartItem from "../components/cart/CartItem.vue"
+import Summary from "../components/cart/sum.vue"
+export default {
+   component:{
+      CartItem ,Summary
+   },
+   computed: {
+      products(){
+         return this.$store.getters.cartItems
+      }
+   }
+}
+</script>
