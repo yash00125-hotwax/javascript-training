a51bb49868534ff928836f42ab921da4b4844058
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-16 14:00:50 +0530

    cart sum
    this file will give the result of the total cart amount 
    cart_total() is a computed property that is getting the information from the state cart 
    cart_total is also a  getter in store as  well so that gives the information here

---
+++ Yash_E-com/SRC/components/cart/CARTSUMM.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="cart-item">  
+        <h3> CART TOTAL: {{cart_total }} Ruppes </h3>
+        
+        <button class="view-jersey" @click="done()"> Pay </button>
+    </div>
+</template>>
+
+<script>
+export default {
+    props: ['jersey'],
+    computed:{
+        cart_total(){
+            return this.$store.getters.cart_Total
+        }
+    }, 
+    methods:{
+        done(){
+            return console.log("yor payemnet is done thank you")
+        }
+    }
+}
+</script>
+
