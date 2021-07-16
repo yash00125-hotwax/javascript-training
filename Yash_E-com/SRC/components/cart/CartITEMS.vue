57ba9e37f0f315d2c94fbea4d3b26ff238c9e9df
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-16 13:57:19 +0530

    cart item.vue
    this file is for cartitems also this file includes the information about what jersey has been picked by the user
    also this constitutes of information  individual jersey is selected how many times and gives us a 
    calculated  result and it is a  computed property

---
+++ Yash_E-com/SRC/components/cart/CartITEMS.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class = "cart-item">
+          <div class="head">
+                   
+                   <h3>  {{ jersey.name}} </h3>   <!-- as im passing through jersey n-->
+                   <h4>  IN Cart : {{jersey.quantity}} </h4>
+                   <h4>  Total Cost: {{ item_cost}}     </h4>
+
+          </div>
+          <p> {{ description }}</p>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['jersey'],
+    computed: {
+        description(){
+            return this.jersey.description.substring(0,100)
+
+
+        },
+        item_cost(){
+            return this.jersey.price * this.jersey.quantity
+        }
+    }
+}
+</script>
+<style lang ="scss">
+.cart-item{
+    width:90%;
+    left:5%;
+    margin: 10%;
+    background-color: palegoldenrod;
+    box-shadow:  0 0 5px darkolivegreen;
+    border-radius: 5px;
+    padding: 10px;
+    text-align: center;
+
+    .head{
+       
+       display: flex;
+       justify-content: space-around;
+      
+    }
+
+
+}  
+
+</style>
