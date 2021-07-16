b930bdee6b1799aa6c8234232f2c0e51f49ffa59
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-16 15:10:10 +0530

    cart.vue
    this file contains all the information that will be shown on cart page 
    also jerseys() is a computed property which return the information form getter cart items which in turns gets the information of the state cart which is locally updated

---
+++ Yash_E-com/SRC/views/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1> YOUR CART</h1>
+     <!--   <div v-for="jersey in jerseys" :key="jersey.id" >  {{ jersey.name}}</div> -->
+         <CartITEMS
+            v-for="jersey in jerseys" 
+            :key="jersey.id"
+            :jersey="jersey"
+          />
+
+         <CartSUM
+           :jersey="jersey"
+        
+        />   
+    </div>    <!--as everything is updated frim the store-->
+</template>
+<script>
+
+ import CartITEMS  from '../components/cart/Cartitem.vue' 
+ import CartSUM  from '../components/cart/Cartsum.vue' 
+
+export default {
+    components:{
+        CartITEMS , CartSUM
+
+    },
+    computed:{
+        jerseys(){
+            return this.$store.getters.CartITEMS
+        }
+    }
+}
+</script>
