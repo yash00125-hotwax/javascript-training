3fdc13917ac931c51768c1ba9aaf16b2c47b7888
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-16 15:06:50 +0530

    about page
    this file contains simple information about the developer

---
+++ Yash_E-com/SRC/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>    this website is made by yash gaikwad  </p>
+    <p>    this website is dedicated to all fans who wants to support thier teams  </p>
+    <p>    hope you have a good time shopping  </p>
+
+
+    <footer class="last"> MADE BY : Yash Gaikwad   </footer>
+  </div>
+
+
+  
+
+  
+</template>
+<style lang="scss">
+.last{
+   bottom:0%; 
+
+}
+
+
+</style>
