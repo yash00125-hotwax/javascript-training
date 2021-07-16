fb05192935388d60567da6249bf7b09063dc66ba
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-16 15:02:56 +0530

    Create router-index.js
    very basic file contains the routes of the application

---
+++ Yash_E-com/SRC/router/router-index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Cart from  '../views/Cart.vue'
+import About from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',  
+    name: 'Home',
+    component: Home
+
+  },
+  {
+    path: '/cart',  
+    name: 'Cart',
+    component: Cart 
+
+  },
+  {
+    path: '/about',  
+    name: 'About',
+    component: About 
+
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
