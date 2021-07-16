24ee8cd11e119fcaea4272e40fa6e4518228cd56
yash00125-hotwax <84585503+yash00125-hotwax@users.noreply.github.com>
2021-07-16 15:01:38 +0530

    contains items only

---
+++ Yash_E-com/SRC/data/item.js
@@ -0,0 +1,128 @@
+
+
+
+
+const items = [
+    {
+         
+        id: 1,
+        name: 'Real Madrid ',
+        imgsrc : '../assets/real.jpeg', 
+        price: 4999,
+        category : 'Footbal Jersyes',
+        description: "RealMadrid: meaning Royal Madrid Football Club), commonly referred to as Real Madrid, is a Spanish professional football club based in Founded on 6 March 1902 as Madrid Football Club, the club has traditionally worn a white home kit since inception"
+
+    },
+    {
+        
+        id: 2,
+        name: 'Arsenal',
+        price: 4999,
+        
+        category : 'Footbal Jersyes',
+        description: "Arsenal: Arsenal Football Club is a professional football club based in London Arsenal was the first club from the South of England to join the Football League, in 1893, and they reached the First Division in 1904. Relegated only once, in 1913, they continue the longest streak in the top division,[6] and have won the second-most top-flight matches in English football history."
+
+    },
+    {
+        
+        id: 3,
+        name: 'Bayern Munich ',
+        price: 4999,
+        
+        category : 'Footbal Jersyes',
+        description: "BayernMunich : Fußball-Club Bayern München e. V , Bayern is historically the most successful team in German football, as they have won the most championships and the most cups,  They are also Germany's most successful team in international competitions, having won fourteen trophies. Bayern is one of only five clubs to have won all three major European competition "
+
+    },
+    {
+        
+        id: 4,
+        name: 'Paris Saint Germain',
+        price: 4999,
+        
+        category : 'Footbal Jersyes',
+        description: "PSG : Founded in 1970, the Parisians won their first major honour, the French Cup, in 1982 and their first Ligue 1 title in 1986. The 1990s was among the most successful periods in PSG's history; they claimed a second league, three French Cups, PSG are the club with most consecutive seasons playing in the top-flight and one of only two French clubs to have won a major European title. They are also the most popular football club in France and one of the most widely supported teams in the world. "
+
+    },
+    {
+        
+        id: 5,
+        name: 'Ac Milan',
+        
+        price: 4999,
+        category : 'Footbal Jersyes',
+        description: "AcMilan: Associazione Calcio Milan, commonly referred to as AC Milan or simply Milan, is a professional football club in Milan, Italy, founded in 1899, Milan's home games are played at San Siro, also known as the Stadio Giuseppe Meazza. The stadium, which is shared with city rivals Internazionale, is the largest in Italian football, with a total capacity of 75,923 "
+
+    },
+    {
+        
+        id: 6,
+        name: 'Juventus',
+        price: 4999,
+        
+        category : 'Footbal Jersyes',
+        description: "Juventus: Juventus Football Club, s a professional football club based in Turin, Piedmont, Italy, that competes in the Serie A, the top tier of the Italian football league system. Founded in 1897 by a group of Torinese students, the club has worn a black and white striped home kit since 1903 and has played home matches in different grounds around its city, the latest being the 41,507-capacity Juventus Stadium. Nicknamed Vecchia Signora  the club has won 36 official league titles, 14 Coppa Italia titles and nine Supercoppa Italiana titles, being the record holder for all these competitions "
+
+    },
+    {
+        
+        id: 7,
+        name: 'Chelsea',
+        price: 4999,
+        
+        category : 'Footbal Jersyes',
+        description: "Chelsea: Chelsea Football Club is an English professional football club based in Fulham, West London, Chelsea are one of five clubs to have won all three of UEFA's main club competitions, the first English club to achieve the UEFA treble, the only London club to have won the Champions League and the only club to have won all three major European competitions twice."
+
+    },
+    {
+        
+        id: 8,
+        name: 'Barecelona',
+        price: 4999,
+        
+        category : 'Footbal Jersyes',
+        description: "FC Barcelona : Founded in 1899 by a group of Swiss, Spanish, and English footballers led by Joan Gamper, the club has become a symbol of Catalan culture and Catalanism, Barcelona is one of the most widely supported teams in the world, and the club has one of the largest social media following in the world among sports teams.  "
+
+    },  
+    {
+        
+        id: 9,
+        name: 'Borruisa Dortmond',
+        
+        price: 4999,
+        category : 'Footbal Jersyes',
+        description: "BVB: Ballspielverein Borussia 09 e. V. Dortmund, commonly known as Borussia Dortmund, Founded in 1909 by eighteen football players from Dortmund, the football team is part of a large membership-based sports club with more than 145,000 members,[3] making Borussia Dortmund the second largest sports club by membership in Germany. "
+
+    },
+    {
+        
+        id: 10,
+        name: 'Manchester United',
+        price: 4999,
+        
+        category : 'Footbal Jersyes',
+        description: "Manchester United : Founded in 1909 by eighteen football players from Dortmund, the football team is part of a large membership-based sports club with more than 145,000 members,[3] making Borussia Dortmund the second largest sports club by membership in Germany, Manchester United was the highest-earning football club in the world for 2016–17, with an annual revenue of €676.3 million "
+
+    },
+    {
+        
+        id: 11,
+        name: 'Inter Milan',
+        price: 4999,
+        
+        category : 'Footbal Jersyes',
+        description: "Intermilan: Football Club Internazionale Milano, commonly referred to as Internazionale, Founded in 1908 following a schism within the Milan Cricket and Football Club, Inter won its first championship in 1910. Since its formation, the club has won 31 domestic trophies, including 19 league titles, 7 Coppa Italia and 5 Supercoppa Italiana. From 2006 to 2010, the club won five successive league titles, equalling the all-time record at that time "
+
+    },
+    {
+        
+        id: 12,
+        name: 'AS Monanco',
+        price: 4999,
+        
+        category : 'Footbal Jersyes',
+        description: "Monaco: Association Sportive de Monaco Football Club SA, commonly referred to as AS Monaco, Unlike several other European microstates, Monaco has never organized a domestic league and has never sought separate membership in either UEFA or FIFA. As a result, AS Monaco has no domestic league to play in its home country, resulting in it being expatriated into the French league system.  "
+
+    },
+
+]
+export default items
